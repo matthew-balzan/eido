58789de1385e80685b2be2f39938398dab59588a
MatthewBalzan <matthewb487@gmail.com>
2024-10-16 18:53:43 +0200

    fix early EOF, maybe fix websocket closed skip

--- internal/models/const.go
+++ internal/models/const.go
@@ -3,6 +3,6 @@ package models
 const ColorError int = 15548997
 const ColorDefault int = 10181046
 
-const TimeoutSecondsDisconnect int64 = 86400
+const TimeoutSecondsDisconnect int64 = 1000
 
 const MaxQueueLength int = 100
